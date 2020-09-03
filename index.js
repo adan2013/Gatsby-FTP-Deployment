f3b57265143bf1ee86a85e387639e956466b6c91
Daniel Alberski <danielalberski@gmail.com>
2020-09-03 20:37:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+
+const fs = require("fs")
+const exec = require('child_process').exec
+const dircompare = require('dir-compare')
+
+const runCommand = (cmd, dir, muteErrors, callback) => {
+    const proc = exec(cmd, { cwd: dir })
+    proc.stdout.on('data', data => console.log(data))
+    if(!muteErrors) proc.stderr.on('data', data => console.error(data))
+    proc.on('exit', code => callback(code === 0))
+}
+
+const reportSuccess = (message) => console.log('\x1b[33m%s\x1b[0m', message)
+
+const reportNextStep = (message) => console.log('\x1b[36m%s\x1b[0m', message)
+
+const reportError = (error) => {
+    console.log('\x1b[31mERROR: %s\x1b[0m', error)
+    process.exit(1)
+}
+
+
+
+function print(result) {
+    console.log('Directories are %s', result.same ? 'identical' : 'different')
+    console.log('Statistics - equal entries: %s, distinct entries: %s, left only entries: %s, right only entries: %s, differences: %s',
+        result.equal, result.distinct, result.left, result.right, result.differences)
+}
+
+
+
+
+reportNextStep('Pulling changes...')
+runCommand('git pull', './repo', false, gitResult => {
+    if(gitResult) {
+        reportNextStep('Installing dependencies...')
+        runCommand('npm install', './repo', true, npmResult => {
+            if(npmResult) {
+                reportNextStep('Building Gatsby website...')
+                runCommand('gatsby build', './repo', false, gatsbyResult => {
+                    if(gatsbyResult) {
+                        fs.mkdir('./currentFTP', { recursive: true }, (mkdirError) => {
+                            if(!mkdirError) {
+                                reportNextStep('Comparing build with previous data...')
+                                const difference = dircompare.compareSync('./repo/public', './currentFTP', {
+                                    compareSize: true,
+                                    compareDate: true
+                                })
+                                if(!difference.same) {
+                                    //TODO here
+                                }else{
+                                    reportSuccess('Already up to date!')
+                                }
+                            }else{
+                                reportError('mkdir currentFTP')
+                            }
+                        })
+                    }else{
+                        reportError('gatsby build')
+                    }
+                })
+            }else{
+                reportError('npm install')
+            }
+        })
+    }else{
+        reportError('git pull')
+    }
+})
