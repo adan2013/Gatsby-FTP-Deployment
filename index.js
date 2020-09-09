c023d164b4592dcd8899d9f108da3a50f9774130
Daniel Alberski <danielalberski@gmail.com>
2020-09-09 19:06:52 +0200

    code refactoring

--- index.js
+++ index.js
@@ -14,25 +14,37 @@ let emailContent = ''
 let changedFiles = []
 let changedFilesCount = 0
 
-const bgColor = {
-    NONE: '#ffffff',
-    RED: '#b41713',
-    GREEN: '#297933',
-    BLUE: '#0a6a6a'
+const consoleBg = {
+    normal: '',
+    success: '\x1b[33m',
+    error: '\x1b[31m',
+    nextStep: '\x1b[36m'
 }
 
-const addToEmailContent = (message, color = bgColor.NONE) => {
-    if(color === bgColor.NONE) {
-        emailContent += `${message}<br/>`
-    }else{
-        const style = `padding: 2px 5px; background-color: ${color}; color: #ffffff`
-        emailContent += `<div style="${style}">${message}</div>`
+const emailBg = {
+    normal: '#ffffff',
+    success: '#297933',
+    error: '#b41713',
+    nextStep: '#0a6a6a'
+}
+
+const addLeadingZero = number => number < 10 ? '0' + number : number
+
+const log = (message, consoleType = consoleBg.normal, emailType = emailBg.normal) => {
+    if(consoleType || consoleType === consoleBg.normal) console.log(`${consoleType}%s\x1b[0m`, message)
+    if(emailType) {
+        if(emailType === emailBg.normal) {
+            emailContent += `${message}<br/>`
+        }else{
+            const style = `padding: 2px 5px; background-color: ${emailType}; color: #ffffff`
+            emailContent += `<div style="${style}">${message}</div>`
+        }
     }
 }
 
 const sendEmail = async (success, message) => {
-    const globalStyle = `font-family: 'Consolas'`
-    const style = `padding: 2px 5px; background-color: ${success ? bgColor.GREEN : bgColor.RED}; color: #ffffff`
+    const globalStyle = `font-family: 'Consolas', 'Lucida Console'`
+    const style = `padding: 2px 5px; background-color: ${success ? emailBg.success : emailBg.error}; color: #ffffff`
     const t1 = performance.now()
     const diff = (t1 - t0) / 1000
     const min = addLeadingZero(Math.floor(diff / 60))
@@ -50,59 +62,52 @@ const sendEmail = async (success, message) => {
         from: `Gatsby Deployer <${process.env.MAIL_USER}>`,
         to: process.env.MAIL_DESTINATION_ADDRESS,
         subject: success ? `Website successfully deployed` : `Website deploy failed!`,
-        html: `<div style="${globalStyle}"><div style="${style}">${message}</div>Current time: ${new Date().toString()}<br/>Execution time: ${min}:${sec}<br/><br/><b>Console output:</b><br/>${emailContent}</div>`
+        html: `
+            <div style="${globalStyle}">
+                <div style="${style}">${message}</div>
+                Current time: ${new Date().toString()}<br/>
+                Execution time: ${min}:${sec}<br/><br/>
+                <b>Console output:</b><br/>
+                ${emailContent}
+            </div>
+        `
     })
 }
 
-const runCommand = (cmd, dir, muteErrors) => {
-    return new Promise(resolve => {
-        const proc = exec(cmd, { cwd: dir })
-        proc.stdout.on('data', data => {
-            console.log(data)
-            addToEmailContent(data, bgColor.NONE)
-        })
-        if(!muteErrors) proc.stderr.on('data', data => {
-            console.error(data)
-            addToEmailContent(data, bgColor.NONE)
-        })
-        proc.on('exit', code => code === 0 ? resolve() : reportError(`Command "${cmd}"`))
-    })
-}
-
-const addLeadingZero = number => number < 10 ? '0' + number : number
-
-const reportNextStep = message => {
-    console.log('\x1b[36m%s\x1b[0m', message)
-    addToEmailContent(message, bgColor.BLUE)
-}
+const reportNextStep = message => log(message, consoleBg.nextStep, emailBg.nextStep)
 
 const reportSuccess = message => {
     const t1 = performance.now()
     const diff = (t1 - t0) / 1000
     const min = addLeadingZero(Math.floor(diff / 60))
     const sec = addLeadingZero(Math.floor(diff - min * 60))
-    console.log('\x1b[33m%s\x1b[0m', message)
-    console.log(`Current time: ${new Date().toString()}`)
-    console.log(`Execution time: ${min}:${sec}`)
+    log(message, consoleBg.success, null)
+    log(`Current time: ${new Date().toString()}`, consoleBg.normal, null)
+    log(`Execution time: ${min}:${sec}`, consoleBg.normal, null)
     sendEmail(true, message).then(() => process.exit(0))
 }
 
 const reportError = (error) => {
-    console.log('\x1b[31mERROR: %s\x1b[0m', error)
+    log(`ERROR: ${error}`, consoleBg.error, emailBg.normal)
     sendEmail(false, error).then(() => process.exit(1))
+}
+
+const runCommand = (cmd, dir, muteErrors) => {
+    return new Promise(resolve => {
+        const proc = exec(cmd, { cwd: dir })
+        proc.stdout.on('data', data => log(data))
+        if(!muteErrors) proc.stderr.on('data', data => log(data))
+        proc.on('exit', code => code === 0 ? resolve() : reportError(`Command "${cmd}"`))
+    })
 }
 
 const pullGitRepository = () => {
     reportNextStep('Pulling changes...')
     return new Promise(resolve => {
         runCommand('git pull', './repo', false).then(() => {
-            const txt = `Current commit:`
-            console.log(txt)
-            addToEmailContent(txt, bgColor.NONE)
-            runCommand('git log -1', './repo', false).then(() => {
-                resolve()
-            }).catch(() => reportError('git log -1'))
-        }).catch(() => reportError('git pull'))
+            log(`Current commit:`)
+            runCommand('git log -1', './repo', false).then(() => resolve())
+        })
     })
 }
 
@@ -123,12 +128,9 @@ const compareBuildWithPreviousData = () => {
             fs.mkdirSync('./currentFTP', { recursive: true })
             const diff = dircompare.compareSync('./repo/public', './currentFTP', {
                 compareSize: true,
-                compareContent: true,
-                //compareDate: true,
+                compareContent: true
             })
-            const txt = `Statistics: equal: ${diff.equal}, distinct: ${diff.distinct}, new: ${diff.left}, old: ${diff.right}`
-            console.log(txt)
-            addToEmailContent(txt, bgColor.NONE)
+            log(`Statistics: equal: ${diff.equal}, distinct: ${diff.distinct}, new: ${diff.left}, old: ${diff.right}`)
             resolve(diff)
         }catch (e) {
             reportError(`diff-compare (message: ${e})`)
@@ -140,22 +142,17 @@ const syncFtpServer = changes => {
     reportNextStep('Connecting to the FTP server...')
     return new Promise(resolve => {
         const ftp = new basicFtp.Client()
-        try {
-            ftp.access({
-                host: process.env.FTP_HOST,
-                user: process.env.FTP_USER,
-                password: process.env.FTP_PASSWORD
-            }).then(() => {
-                console.log('Connected!')
-                addToEmailContent('Connected!', bgColor.NONE)
-                reportNextStep('Uploading files...')
-                changedFiles = changes.diffSet.filter(obj => ['left', 'right', 'distinct'].indexOf(obj.state) >= 0)
-                changedFilesCount = changedFiles.length
-                nextFile(ftp, () => resolve())
-            })
-        }catch (e) {
-            reportError(`Critical FTP connection error (message: ${e})`)
-        }
+        ftp.access({
+            host: process.env.FTP_HOST,
+            user: process.env.FTP_USER,
+            password: process.env.FTP_PASSWORD
+        }).then(() => {
+            log('Connected!')
+            reportNextStep('Uploading files...')
+            changedFiles = changes.diffSet.filter(obj => ['left', 'right', 'distinct'].indexOf(obj.state) >= 0)
+            changedFilesCount = changedFiles.length
+            nextFile(ftp, () => resolve())
+        }).catch(err => reportError(`Critical FTP connection error (message: ${err})`))
     })
 }
 
@@ -187,16 +184,12 @@ const uploadFileToFtp = (ftp, file, callback) => {
     const destination = path.join(file.relativePath, file.name1).replace(/\\/g, '/')
     const isFile = fs.statSync(source).isFile()
     if(isFile) {
-        const txt = `${getUploadPercent()} UPLOAD "${destination}"`
-        console.log(txt)
-        addToEmailContent(txt, bgColor.NONE)
+        log(`${getUploadPercent()} UPLOAD "${destination}"`)
         ftp.uploadFrom(source, destination).then(() => {
             callback()
         }).catch(() => reportError(`(ftp-put) "${source}" >>> "${destination}"`))
     }else{
-        const txt = `${getUploadPercent()} MKDIR  "${destination}"`
-        console.log(txt)
-        addToEmailContent(txt, bgColor.NONE)
+        log(`${getUploadPercent()} MKDIR  "${destination}"`)
         ftp.ensureDir(destination).then(() => {
             ftp.cd('/').then(() => {
                 callback()
@@ -210,19 +203,15 @@ const deleteFileFromFtp = (ftp, file, callback) => {
     const target = path.join(file.relativePath, file.name2).replace(/\\/g, '/')
     const isFile = fs.statSync(targetOnPc).isFile()
     if(isFile) {
-        const txt = `${getUploadPercent()} DELETE "${target}"`
-        console.log(txt)
-        addToEmailContent(txt, bgColor.NONE)
+        log(`${getUploadPercent()} DELETE "${target}"`)
         ftp.remove(target).then(() => callback())
     }else{
-        const txt = `${getUploadPercent()} RMDIR  "${target}"`
-        console.log(txt)
-        addToEmailContent(txt, bgColor.NONE)
+        log(`${getUploadPercent()} RMDIR  "${target}"`)
         ftp.removeDir(target).then(() => callback())
     }
 }
 
-const saveCurrentSave = () => {
+const saveCurrentState = () => {
     reportNextStep('Saving current state...')
     return new Promise(resolve => {
         fs.rmdir('./currentFTP', { recursive: true }, err => {
@@ -245,7 +234,7 @@ pullGitRepository().then(() => {
                     reportSuccess('Already up to date!')
                 }else{
                     syncFtpServer(diff).then(() => {
-                        saveCurrentSave().then(() => {
+                        saveCurrentState().then(() => {
                             reportSuccess('Website deployed!')
                         })
                     })
